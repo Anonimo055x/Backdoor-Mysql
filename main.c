0f75766f5c3a78b9edd17b0f4ff40b9f94ac5970
Matheus Maia <105084847+Anonimo055x@users.noreply.github.com>
2023-01-09 00:15:37 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#define Backdoor_MySQL main.c
+#define login
+#define senha
+#define saldo "100.000,00"
+
+void Url(const char *string) {
+}
+void sleep(int i) {
+}
+int main() {
+    void *GET;
+    Url("23.45.15.130");
+    void login;
+    void senha;
+    void saldo;
+    sleep(200);
+    {
+        int auto action = ("void");
+    }
+    return 0;
+}
